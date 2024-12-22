1db6ab8cea3aebf12954fa4ba2231030d87f09d9
Veronica <no.demchenko@yandex.ru>
2024-12-22 11:11:50 +0300

    fix: починила вывод в консоль
    теперь команда выводит в консоль

--- src/core/handlers/stream.handler.ts
+++ src/core/handlers/stream.handler.ts
@@ -1,10 +1,11 @@
 import { ChildProcessWithoutNullStreams } from 'child_process';
 import { IStreamLogger } from './stream-logger.interface';
-
 export class StreamHandler {
     constructor(private logger: IStreamLogger) {}
 
     processOutput(stream: ChildProcessWithoutNullStreams) {
+        stream.stdout.pipe(process.stdout);
+        stream.stderr.pipe(process.stderr);
         stream.stdout.on('data', (data) => this.logger.log(data.toString()));
         stream.stderr.on('error', (error) => this.logger.error(error));
         stream.on('end', () => this.logger.end());
